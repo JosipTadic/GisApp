fca4805cb58554c63ca58db3c40f1f82bb45e529
JosipTadic <josip.tadic@uniline.hr>
2021-08-13 21:51:36 +0200

    enabled changing tile layers

--- src/Components/MapComponent.tsx
+++ src/Components/MapComponent.tsx
@@ -1,23 +1,39 @@
 import React from "react";
-import { LatLngExpression } from "leaflet";
+import * as L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, FeatureGroup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  FeatureGroup,
+  LayersControl,
+} from "react-leaflet";
 import MarkerComponent from "./MarkerComponent";
 import { EditControl } from "react-leaflet-draw";
+import "leaflet-draw";
 
 const MapComponent = () => {
-  const position: LatLngExpression = [44.9275, 13.80361];
+  const position: L.LatLngExpression = [44.9275, 13.80361];
   const zoom: number = 14;
 
   return (
     <>
       <MapContainer center={position} zoom={zoom} scrollWheelZoom={false}>
-        <TileLayer
-          attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <MarkerComponent />
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="OpenStreetMap Classic">
+            <TileLayer
+              attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="OpenStreetMap Black and White">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
 
+        <MarkerComponent />
         <FeatureGroup>
           <EditControl position="topleft" draw={{}} />
         </FeatureGroup>
